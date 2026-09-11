c1230acce66529615de533ce2a008a62b509f371
Ethan John <c.ethanjohn@gmail.com>
2026-09-11 22:27:35 +0400

    blink

--- src/main.c
+++ src/main.c
@@ -1,25 +1,23 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
+#include "pico/stdlib.h"
+#include <stdio.h>
 
+int main() {
+	stdio_init_all();
 
+	// Initialise the Wi-Fi chip
+	if (cyw43_arch_init()) {
+		printf("Wi-Fi init failed\n");
+		return -1;
+	}
 
+	// Example to turn on the Pico W LED
+	cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
 
-int main()
-{
-    stdio_init_all();
-
-    // Initialise the Wi-Fi chip
-    if (cyw43_arch_init()) {
-        printf("Wi-Fi init failed\n");
-        return -1;
-    }
-
-    // Example to turn on the Pico W LED
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-    }
+	while (true) {
+		cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+		sleep_ms(1000);
+		cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+		sleep_ms(1000);
+	}
 }
