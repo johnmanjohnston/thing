399a1bcfd99f6009fa1f17dc257ebf1f2e99f028
Ethan John <c.ethanjohn@gmail.com>
2026-09-13 01:27:53 +0400

    http routing system

--- src/main.c
+++ src/main.c
@@ -10,10 +10,27 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define CMD_NULL 0x0
+#define CMD_TOGGLE 0x1
+#define CMD_DOWN 0x2
+#define CMD_UP 0x3
+
 static const char* header =
 	"HTTP/1.1 200 OK\r\nContent-type: text/html\r\n\r\n";
-static const char* webpage = "<!DOCTYPE html> <html><head><title>super cool "
-							 "webpage aaa %d</title></head></html>";
+static const char* webpage =
+	"<!DOCTYPE html> <html><head><title>super cool "
+	"webpage aaa %d</title></head><body>beans: %s</body></html>";
+
+int parse_route(char* route) {
+	if (strncmp(route, "GET /tog", 8) == 0)
+		return CMD_TOGGLE;
+	if (strncmp(route, "GET /inc", 8) == 0)
+		return CMD_UP;
+	if (strncmp(route, "GET /dec", 8) == 0)
+		return CMD_DOWN;
+
+	return CMD_NULL;
+}
 
 static err_t recv_cb(void* arg, struct tcp_pcb* pcb, struct pbuf* p, err_t e) {
 	if (p == NULL) {
@@ -21,14 +38,23 @@ static err_t recv_cb(void* arg, struct tcp_pcb* pcb, struct pbuf* p, err_t e) {
 		return ERR_CLSD;
 	}
 
-	char buf[256];
-	snprintf(buf, sizeof(buf), webpage, rand());
+	// check route
+	int cmd = parse_route((char*)p->payload);
 
 	tcp_write(pcb, header, strlen(header), TCP_WRITE_FLAG_COPY);
-	// tcp_write(pcb, webpage, strlen(webpage), TCP_WRITE_FLAG_COPY);
-	tcp_write(pcb, buf, strlen(buf), TCP_WRITE_FLAG_COPY);
-	tcp_output(pcb);
 
+	if (cmd == CMD_NULL) {
+		char buf[512];
+		snprintf(buf, sizeof(buf), webpage, cmd, (char*)p->payload);
+
+		// tcp_write(pcb, webpage, strlen(webpage), TCP_WRITE_FLAG_COPY);
+		tcp_write(pcb, buf, strlen(buf), TCP_WRITE_FLAG_COPY);
+
+	} else if (cmd == CMD_TOGGLE) {
+		tcp_write(pcb, "ok", 2, TCP_WRITE_FLAG_COPY);
+	}
+
+	tcp_output(pcb);
 	tcp_recved(pcb, p->tot_len);
 	pbuf_free(p);
 	tcp_close(pcb);
